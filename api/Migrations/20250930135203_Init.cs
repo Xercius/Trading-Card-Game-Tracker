fa834f7352622451f36f6570f2f1adc7037760ec
Xercius <75197226+Xercius@users.noreply.github.com>
2025-09-30 06:55:30 -0700

    tests still not working

--- api/Migrations/20250930135203_Init.cs
+++ api/Migrations/20250930135203_Init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace api.Migrations
 {
     /// <inheritdoc />
-    public partial class FreshInit : Migration
+    public partial class Init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
--- api/Migrations/AppDbContextModelSnapshot.cs
+++ api/Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace api.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
+            modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
 
             modelBuilder.Entity("api.Models.Card", b =>
                 {
